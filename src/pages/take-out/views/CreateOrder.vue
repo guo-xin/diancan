a116f173ab28397ee94af050eec0ede806d9b001
lidong <lidong@qfpay.com>
2017-04-01 20:09:58 +0800

    change

--- src/pages/take-out/views/CreateOrder.vue
+++ src/pages/take-out/views/CreateOrder.vue
@@ -57,6 +57,7 @@
 
 <script>
   /* global _hmt */
+  /* esling-diasble  */
   import Config from '../../../methods/Config'
   import Util from '../../../methods/Util'
   import alert from '../components/alert/alert.vue'
