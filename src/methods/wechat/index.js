35d0b71221d93b5d814c7890441873b0110c252e
lidong <lidong@qfpay.com>
2017-05-27 10:44:37 +0800

    split verify

--- src/methods/wechat/index.js
+++ src/methods/wechat/index.js
@@ -5,81 +5,6 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
 
-const getAuthInfo = (mchtId) => {
-  return new Promise((resolve, reject) => {
-    Vue.http({
-      method: 'jsonp',
-      url: `${Config.apiHost}diancan/c/takeauthinfo`,
-      params: {
-        mchnt_id: mchtId,
-        format: 'jsonp'
-      }
-    })
-    .then((res) => {
-      let data = res.data
-      if (data.respcd === Config.code.OK) {
-        resolve(data.data)
-      } else {
-        window.alert(data.resperr)
-      }
-    })
-    .catch(() => {
-      window.alert('网络错误!')
-    })
-  })
-}
-
-const getWeixinBack = (data) => {
-  return new Promise((resolve, reject) => {
-    Vue.http({
-      method: 'jsonp',
-      url: `${Config.apiHost}diancan/weixincallback`,
-      params: {
-        appid: data.appid,
-        format: 'jsonp'
-      }
-    })
-    .then((res) => {
-      let data = res.data
-      if (data.respcd === Config.code.OK) {
-        window.localStorage.setItem('dc_appid', res.data.data.appid)
-        window.localStorage.setItem('dc_openid', res.data.data.openid)
-        resolve()
-      } else {
-        reject(data)
-        let url = `${Config.o2Host}trade/v1/customer/get?appid=${data.data.appid}&redirect_uri=` + encodeURIComponent(window.location.origin + window.location.pathname + window.location.search)
-        window.location.replace(url)
-      }
-    })
-    .catch((err) => {
-      console.log('获取微信回调错误', err)
-    })
-  })
-}
-
-const getVerify = async (mchtId) => {
-  let data = await getAuthInfo(mchtId)
-  await getWeixinBack(data)
-}
-
-const verify = async () => {
-  let hashArray, tempHashMchtId
-  let LSArray, tempLSMchtId
-
-  if (window.location.hash) {
-    window.localStorage.setItem('redirect_uri', window.location.hash)
-    hashArray = window.location.hash.split('/')
-    tempHashMchtId = hashArray[1] === 'merchant' ? hashArray[2] : hashArray[3]
-  }
-  if (window.localStorage.getItem('redirect_uri')) {
-    LSArray = window.localStorage.getItem('redirect_uri').split('/')
-    tempLSMchtId = LSArray[1] === 'merchant' ? LSArray[2] : LSArray[3]
-  }
-  let mchtId = tempHashMchtId || tempLSMchtId
-
-  await getVerify(mchtId)
-}
-
 /* global wx */
 const DEBUG = false
 const jsApiList = [
@@ -119,7 +44,6 @@ const getWxConfig = (args, jsApis = jsApiList) => {
         signature: wxData.signature,  // 必填，签名，见附录1
         jsApiList: jsApis // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
       }
-      wxData.url = window.location.href
       wx.config(wxargs)
     })
     .catch((err) => {
@@ -220,7 +144,6 @@ const hideAllNonBaseMenuItem = () => {
 }
 
 export default {
-  verify,
   init,
   ready,
   hideOptionMenu,
