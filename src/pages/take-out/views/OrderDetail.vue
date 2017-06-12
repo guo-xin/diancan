fe21d7472f4a4d83633b1c4cc7ed83d6da3f6f6f
liuyuan <fenjin1987@gmail.com>
2017-06-12 12:22:27 +0800

    添加order status 4 订单取消状态

--- src/pages/take-out/views/OrderDetail.vue
+++ src/pages/take-out/views/OrderDetail.vue
@@ -72,10 +72,11 @@
   import Util from '../../../methods/Util'
   import Loading from '../../../components/loading/Loading'
   import Config from '../../../methods/Config'
-  import img0 from '../assets/img_progress_0_white_label@2x.png'
-  import img1 from '../assets/img_progress_1_white_label@2x.png'
-  import img2 from '../assets/img_progress_2_white_label@2x.png'
-  import img3 from '../assets/img_progress_3_white_label@2x.png'
+  import img0 from '../assets/img_progress_0@2x.png'
+  import img1 from '../assets/img_progress_1@2x.png'
+  import img2 from '../assets/img_progress_2@2x.png'
+  import img3 from '../assets/img_progress_3@2x.png'
+  // import img4 from '../assets/img_progress_4@2x.png'
   //  import Api from '../api/mock'
 
   export default {
