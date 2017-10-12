dac36acbbccfceda22392f765334b4e3a0eb2ce0
hujiajia <hujiajia@cpe-172-100-108-200.twcny.res.rr.com>
2017-10-12 12:06:41 +0800

    减了空格

--- src/pages/take-out/views/CreateOrder.vue
+++ src/pages/take-out/views/CreateOrder.vue
@@ -51,7 +51,7 @@
         <em v-else-if="prepaid.balance < cartData.price" class="red">余额不足</em>
         <i v-else class="icon-checked"></i>
       </div>
-      <div class="chuzhi"  @click="goChuzhi()">
+      <div class="chuzhi" @click="goChuzhi()">
         <p>储值优惠，最高送{{prepaid.max_present_amt | formatCurrency | noZeroCurrency}}元</p><i class="icon-right-arrow"></i>
       </div>
     </section>
