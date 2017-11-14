0573957a1c431bde305af48050ccfcbe8abd78be
hujiajia <hujiajia@cpe-172-100-108-21.twcny.res.rr.com>
2017-11-14 14:54:49 +0800

    购物车中满多少配送用rule的数据

--- src/pages/take-out/components/CartBar.vue
+++ src/pages/take-out/components/CartBar.vue
@@ -16,8 +16,8 @@
       </div>
       <a class="row-status gray-status" v-if="overtime || nodelivery">{{calcBtnText}}</a>
       <span v-else>
-        <a class="row-status" @click.prevent="goNextView()" v-if="deliver.start_delivery_fee <= cartData.price">选好了</a>
-        <a class="row-status gray-status" v-else>{{deliver.start_delivery_fee | formatCurrency | noZeroCurrency}}元起送</a>
+        <a class="row-status" @click.prevent="goNextView()" v-if="rule && rule[0].start_delivery_fee <= cartData.price">选好了</a>
+        <a class="row-status gray-status" v-else>{{rule && rule[0].start_delivery_fee | formatCurrency | noZeroCurrency}}元起送</a>
       </span>
     </div>
     <!--列表-->
