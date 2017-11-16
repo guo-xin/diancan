4a11cbef013a5727506a5e0d51bb2193a5a80431
hujiajia <hujiajia@cpe-172-100-108-21.twcny.res.rr.com>
2017-11-16 20:39:38 +0800

    用户没设置满减，且没开通阶梯配送费

--- src/pages/take-out/views/CreateOrder.vue
+++ src/pages/take-out/views/CreateOrder.vue
@@ -26,7 +26,7 @@
       <div class="deliver-fee" v-if="!current_addr.overdist">
         <em>配送费
           <span v-if="deliverFee.min_shipping_fee && deliverFee.min_shipping_fee <= this.goodsAmt && !deliveryStatus">（满{{deliverFee.min_shipping_fee | formatCurrency | noZeroCurrency}}元免配送费）</span>
-          <span v-if="deliverFee.min_shipping_fee!==0 && !deliverFee.min_shipping_fee && !deliveryStatus">（满{{deliver.min_shipping_fee | formatCurrency | noZeroCurrency}}元免配送费）</span>
+          <span v-if="deliverFee.min_shipping_fee!==0 && !deliverFee.min_shipping_fee && !deliveryStatus && deliver.min_shipping_fee !== 0">（满{{deliver.min_shipping_fee | formatCurrency | noZeroCurrency}}元免配送费）</span>
         </em>
         <span v-if="deliveryStatus">{{deliveryStatus}}</span>
         <span v-if="!deliveryStatus && deliverFee.min_shipping_fee >= 0" :class="{'except': cartData.price >= deliverFee.min_shipping_fee && deliverFee.min_shipping_fee}"><sub>￥</sub>{{deliverFee.shipping_fee || 0 | formatCurrency}}</span>
