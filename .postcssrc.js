fec4a1e18fdc52e98dd2f7b2d37f315b73c3aee3
liuyuan <fenjin1987@gmail.com>
2018-04-11 22:52:37 +0800

    postcss loader

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   "plugins": {
     // to edit target browsers: use "browserslist" field in package.json
-    "postcss-import": {},
     "autoprefixer": {browsers: ["Android >= 2.3", "iOS >= 4"]}
   }
 }
