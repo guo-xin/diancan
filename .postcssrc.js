5d2e6ee4231af9ccbf6a2e638baa7a82fd8560d9
liuyuan <fenjin1987@gmail.com>
2018-04-11 21:37:16 +0800

    poss loader

---
+++ .postcssrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "plugins": {
+    // to edit target browsers: use "browserslist" field in package.json
+    "postcss-import": {},
+    "autoprefixer": {browsers: ["Android >= 4", "iOS >= 7"]}
+  }
+}
--- build/utils.js
+++ build/utils.js
@@ -50,11 +50,8 @@ exports.cssLoaders = function (options) {
   return {
     css: generateLoaders(),
     postcss: generateLoaders(),
-    less: generateLoaders('less'),
     sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
-    stylus: generateLoaders('stylus'),
-    styl: generateLoaders('stylus')
+    scss: generateLoaders('sass')
   }
 }
 
