8bb7b700c366bb78db8e80d09e371a008aad18c1
guoxin <guoxin@qfpay.com>
2018-04-11 22:14:46 +0800

    前缀修改

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,6 +2,6 @@ module.exports = {
   "plugins": {
     // to edit target browsers: use "browserslist" field in package.json
     "postcss-import": {},
-    "autoprefixer": {browsers: ["Android >= 4", "iOS >= 7"]}
+    "autoprefixer": {browsers: ["Android >= 2.3", "iOS >= 4"]}
   }
 }
